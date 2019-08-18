a33e5610c13c883ddf82e994c25398db775cd14c
chanjessica <jchan_sea@yahoo.com>
2019-08-18 10:40:40 -0700

    fixed home

--- lesson07/assignment/hw7/src/components/home.vue
+++ lesson07/assignment/hw7/src/components/home.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <b-carousel   id="carousel-1"   v-model="slide"  :interval="4000"   controls
-      indicators   background="#ababab"  
+      indicators   background="#ababab"  img-width="640" img-height="480" 
       style="text-shadow: 1px 1px 2px #333;"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <h2> {{message}} </h2>
+      <h2> {{ message }} </h2>
       <!-- Slides image -->
-      <b-carousel-slide 
-        class="d-block img-fluid w-100 MyCustomClass"  v-for="i in imgList" :key="i.id" 
-          :img-src= "i.imgSrc"  alt="i.img" >
-    </b-carousel-slide>
-  </b-carousel>
+      <b-carousel-slide   class="MyCustomClass"
+        v-for="i in imgList" :key="i.id"   :img-src= "i.imgSrc"  alt="i.img">
+      </b-carousel-slide>
+
+    </b-carousel>
 
     <p class="mt-4">
       Slide #: {{ slide }}<br>
@@ -28,20 +28,18 @@
       return {
         slide: 0,
         sliding: null,
-        message: "carousel slider",
+        message: "My Kitchen",
         imgList: [  {id:0, img: "food 1", imgSrc: "http://lorempixel.com/output/food-q-c-640-480-1.jpg"},
                     {id:1, img: "food 2", imgSrc: "http://lorempixel.com/output/food-q-c-640-480-2.jpg"},
                     {id:2, img: "food 3", imgSrc: "http://lorempixel.com/output/food-q-c-640-480-3.jpg"}
                 ]
       };
     },
-    methods:{
-      onSlideStart(){
-        this.slide = true
-      },
-      onSlideEnd() {
-        this.slide = false
-      }
+    onSlideStart(){
+      this.slide = true;
+    },
+    onSlideEnd() {
+      this.slide = false;
     }
 }
 </script>
